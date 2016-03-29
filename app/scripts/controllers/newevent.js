1ed4652f78922cbed642bef7193cb23e5d2246b5
mohit493 <mohityadav0493@gmail.com>
2016-03-29 21:17:44 +0200

    Map Added
    Map added with the search functionality.
    Next step > add to the model to create event

--- app/scripts/controllers/newevent.js
+++ app/scripts/controllers/newevent.js
@@ -15,17 +15,73 @@ angular.module('eventifyApp')
       'Karma'
     ];
 
-        $scope.markerID = 2;
+        $scope.lat = 52.47491894326404;
+        $scope.lngt = -1.8684210293371217;
 
-        $scope.mapCenter = {
-            latitude: 59.38,
-            longitude: 18.02
+        $scope.map = {
+            "center": {
+                "latitude": $scope.lat,
+                "longitude": $scope.lngt
+            },
+            "zoom": 15
+        }; //TODO:  set location based on users current gps location 
+        $scope.marker = {
+            id: 0,
+            coords: {
+                latitude: $scope.lat,
+                longitude: $scope.lngt
+            },
+            options: {
+                draggable: true
+            },
+            events: {
+                dragend: function (marker, eventName, args) {
+
+                    $scope.marker.options = {
+                        draggable: true,
+                        labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+                        labelAnchor: "100 0",
+                        labelClass: "marker-labels"
+                    };
+                }
+            }
         };
+        var events = {
+            places_changed: function (searchBox) {
 
-        //This is required by the directive
-        $scope.markerCenter = {
-            latitude: 59.38,
-            longitude: 18.02
+                var place = searchBox.getPlaces();
+                console.log(place[0].geometry.location.lat());
+
+
+                if (!place || place === 'undefined' || place.length === 0) {
+                    console.log('no place data :(');
+                    return;
+                }
+
+                $scope.lat = place[0].geometry.location.lat();
+                $scope.lngt = place[0].geometry.location.lng();
+
+                $scope.map = {
+                    "center": {
+                        "latitude": $scope.lat,
+                        "longitude": $scope.lngt
+
+                    },
+                    "zoom": 18
+                };
+                $scope.marker = {
+                    id: 0,
+                    coords: {
+                        latitude: place[0].geometry.location.lat(),
+                        longitude: place[0].geometry.location.lng()
+                    }
+                };
+
+            }
+        };
+        $scope.searchbox = {
+            template: 'searchbox.tpl.html',
+            events: events
         };
 
     });
