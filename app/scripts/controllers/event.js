b063fdad1812325ec2c308bdbc8b9af1e40d77a1
Mikko Honkanen <mikko.s.honkanen@aalto.fi>
2016-03-31 10:43:32 +0300

    call changes

--- app/scripts/controllers/event.js
+++ app/scripts/controllers/event.js
@@ -8,115 +8,115 @@
  * Controller of the eventifyApp
  */
 angular.module('eventifyApp')
-  .controller('EventCtrl', function ($scope, EventService, TaskService, $routeParams, $http) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    .controller('EventCtrl', function ($scope, EventService, TaskService, $routeParams, $http) {
+        this.awesomeThings = [
+      'HTML5 Boilerplate'
+      , 'AngularJS'
+      , 'Karma'
     ];
 
-    $scope.linkID = $routeParams.linkID;
+        $scope.linkID = $routeParams.linkID;
 
-    //this name should be changed
-    $scope.getEventByID = function (ID) {
-      $scope.eventStatus = true;
-      EventService.Event.get({
-        id: ID
-      }, function (data) {
-        $scope.event = data;
-        console.log(data);
+        //this name should be changed
+        $scope.getEventByID = function (ID) {
+            $scope.eventStatus = true;
+            EventService.Event.get({
+                id: ID
+            }, function (data) {
+                $scope.event = data;
+                console.log(data);
 
-        //not sure if this should be in MapCtrl
-        $scope.mapCenter = {
-          latitude: data.locationLat,
-          longitude: data.locationLong
+                //not sure if this should be in MapCtrl
+                $scope.mapCenter = {
+                    latitude: data.locationLat
+                    , longitude: data.locationLong
+                };
+
+                //This is required by the directive
+                $scope.markerCenter = {
+                    latitude: data.locationLat
+                    , longitude: data.locationLong
+                };
+
+                //couldn't make "{{event.id}}" work directly in the HTML
+                $scope.id = data.id;
+
+                $scope.attendees = data.attendee;
+
+                $scope.eventStatus = false;
+            }, function (data) {
+                console.log(data);
+            });
         };
 
-        //This is required by the directive
-        $scope.markerCenter = {
-          latitude: data.locationLat,
-          longitude: data.locationLong
+        $scope.getUserByID = function (ID) {
+            var user;
+            $scope.attendees.forEach(function (attendee) {
+                if (attendee.id === ID) {
+                    user = attendee;
+                }
+            });
+            return user;
         };
 
-        //couldn't make "{{event.id}}" work directly in the HTML
-        $scope.id = data.id;
+        $scope.newTask = 'nakki';
 
-        $scope.attendees = data.attendee;
+        $scope.createTask = function (id) {
+            TaskService.NewEventTask.save({}, {
+                    event: "1"
+                    , name: "nakki"
+                }
+                , function (data) {
+                    console.log(data);
+                }
+                , function (data) {
+                    console.log(data);
+                });
+        };
 
-        $scope.eventStatus = false;
-      }, function (data) {
-        console.log(data);
-      });
-    };
+        $scope.createItem = function (linkID) {
+            $http.post('http://clementhamon.com/IDP/public/task/new', {
+                    event: "1"
+                    , name: "nakki"
+                }, {
+                    'headers': {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    }
+                })
+                .then(
+                    function (response) {
+                        // success callback
+                    }
+                    , function (response) {
+                        // failure callback
+                    }
+                );
+        };
 
-    $scope.getUserByID = function (ID) {
-      var user;
-      $scope.attendees.forEach(function (attendee) {
-        if (attendee.id === ID) {
-          user = attendee;
-        }
-      });
-      return user;
-    };
+        /*$scope.createTask = function (id) {
+            TaskService.NewEventTask.save({}, {
+              event: id,
+              name: $scope.newTask
+            }, function (data) {
+              console.log(data);
+            }, function (data) {
+              console.log(data);
+            });
+          };*/
 
-    $scope.newTask = 'nakki';
+        /*Refactored everything to use getEventByID,
+        so this is currently not used.
+        Left it here just in case.*/
+        $scope.getAttendees = function (ID) {
+            $scope.attendeeStatus = true;
+            EventService.EventAttendees.query({
+                id: ID
+            }, function (data) {
+                $scope.attendees = data;
+                $scope.attendeeStatus = false;
+            }, function (data) {
+                console.log(data);
+            });
+        };
 
-    $scope.createTask = function (id) {
-      TaskService.NewEventTask.save({}, JSON.stringify({
-          "event": "1",
-          "name": "nakki"
-        }),
-        function (data) {
-          console.log(data);
-        },
-        function (data) {
-          console.log(data);
-        });
-    };
-
-    $scope.createItem = function (linkID) {
-      $http.post('http://clementhamon.com/IDP/public/task/new', {
-          event: "1",
-          name: "nakki"
-        }, {
-          'headers': {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          }
-        })
-        .then(
-          function (response) {
-            // success callback
-          },
-          function (response) {
-            // failure callback
-          }
-        );
-    };
-
-    /*$scope.createTask = function (id) {
-        TaskService.NewEventTask.save({}, {
-          event: id,
-          name: $scope.newTask
-        }, function (data) {
-          console.log(data);
-        }, function (data) {
-          console.log(data);
-        });
-      };*/
-
-    /*Refactored everything to use getEventByID,
-    so this is currently not used.
-    Left it here just in case.*/
-    $scope.getAttendees = function (ID) {
-      $scope.attendeeStatus = true;
-      EventService.EventAttendees.query({
-        id: ID
-      }, function (data) {
-        $scope.attendees = data;
-        $scope.attendeeStatus = false;
-      }, function (data) {
-        console.log(data);
-      });
-    };
-
-  });
+    });
