9f8d4403a81e1ba88bdfd5cc9b5303bbff90d0fe
mohit493 <mohityadav0493@gmail.com>
2016-04-15 14:41:26 +0200

    Spotify_Playlist added
    Functioning and running the Spotify Playlist

--- app/scripts/controllers/event.js
+++ app/scripts/controllers/event.js
@@ -8,88 +8,99 @@
  * Controller of the eventifyApp
  */
 angular.module('eventifyApp')
-  .controller('EventCtrl', function ($scope, EventService, $routeParams, AuthService) {
+    .controller('EventCtrl', function ($scope, EventService, $routeParams, AuthService) {
 
-    $scope.linkID = $routeParams.linkID;
-    $scope.editing = false;
+        $scope.linkID = $routeParams.linkID;
+        $scope.editing = false;
 
-    $scope.isLogged = AuthService.isLoggedIn();
-    
-    $scope.user = AuthService.currentUser();
+        $scope.isLogged = AuthService.isLoggedIn();
+
+        $scope.user = AuthService.currentUser();
+
+        $scope.SpotifyURI = "";
 
 
-    //this name should be changed
-    $scope.getEventByID = function () {
-      $scope.eventStatus = true;
-      EventService.Event.get({
-        eventId: $scope.linkID
-      }, function (data) {
-        $scope.event = data;
-        console.log(data);
+        //this name should be changed
+        $scope.getEventByID = function () {
+            $scope.eventStatus = true;
+            EventService.Event.get({
+                eventId: $scope.linkID
+            }, function (data) {
+                $scope.event = data;
+                console.log(data);
 
-        //setting bg picture
-        $scope.bgStyle = {
-          'background-image': 'url(' + $scope.event.coverPicture + ')'
+                //setting bg picture
+                $scope.bgStyle = {
+                    'background-image': 'url(' + $scope.event.coverPicture + ')'
+                };
+
+                //not sure if this should be in MapCtrl
+                $scope.mapCenter = {
+                    latitude: data.locationLat,
+                    longitude: data.locationLong
+                };
+
+                //This is required by the directive
+                $scope.markerCenter = {
+                    latitude: data.locationLat,
+                    longitude: data.locationLong
+                };
+
+                //couldn't make "{{event.id}}" work directly in the HTML
+
+                $scope.id = data.id;
+                $scope.Spotify = data.spotifyPlaylist;
+
+
+
+                //       $scope.SetSpotifyURI = function () {
+                $scope.SpotifyURI = 'https://embed.spotify.com/?uri=' + $scope.Spotify;
+                //    };
+                console.log($scope.Spotify);
+                console.log($scope.SpotifyURI);
+
+                $scope.attendees = data.attendee;
+
+                $scope.eventStatus = false;
+
+                //finally save data to service
+                EventService.setEventData(data);
+
+            }, function (data) {
+                console.log(data);
+            });
         };
 
-        //not sure if this should be in MapCtrl
-        $scope.mapCenter = {
-          latitude: data.locationLat,
-          longitude: data.locationLong
+        $scope.getUserByID = function (ID) {
+            var user;
+            $scope.attendees.forEach(function (attendee) {
+                if (attendee.id === ID) {
+                    user = attendee;
+                }
+            });
+            return user;
         };
 
-        //This is required by the directive
-        $scope.markerCenter = {
-          latitude: data.locationLat,
-          longitude: data.locationLong
+        //THIS IS NOT USED
+        $scope.startEditing = function () {
+            $scope.editing = true;
         };
 
-        //couldn't make "{{event.id}}" work directly in the HTML
+        //THIS IS NOT USED
+        $scope.stopEditing = function () {
+            $scope.editing = false;
+            EventService.updateEvent.update({}, {
+                id: $scope.event.id,
+                name: $scope.event.name,
+                date: $scope.event.date,
+                time: $scope.event.time,
+                duration: $scope.event.duration,
+                description: $scope.event.description
+            }, function (data) {
+                console.log(data);
+            }, function (data) {
+                console.log(data);
+            });
+        };
 
-        $scope.id = data.id;
-
-        $scope.attendees = data.attendee;
-
-        $scope.eventStatus = false;
-
-        //finally save data to service
-        EventService.setEventData(data);
-
-      }, function (data) {
-        console.log(data);
-      });
-    };
-
-    $scope.getUserByID = function (ID) {
-      var user;
-      $scope.attendees.forEach(function (attendee) {
-        if (attendee.id === ID) {
-          user = attendee;
-        }
-      });
-      return user;
-    };
-
-    //THIS IS NOT USED
-    $scope.startEditing = function () {
-      $scope.editing = true;
-    };
-
-    //THIS IS NOT USED
-    $scope.stopEditing = function () {
-      $scope.editing = false;
-      EventService.updateEvent.update({}, {
-        id: $scope.event.id,
-        name: $scope.event.name,
-        date: $scope.event.date,
-        time: $scope.event.time,
-        duration: $scope.event.duration,
-        description: $scope.event.description
-      }, function (data) {
-        console.log(data);
-      }, function (data) {
-        console.log(data);
-      });
-    };
-
-  });
+    });
