57492ae4af136d16f62417aaf8477594fab68dbe
Mikko Honkanen <mikko.s.honkanen@aalto.fi>
2016-04-20 09:07:33 +0200

    Revert "Revert "newevent merge conflict fixed""
    This reverts commit 93b1ecb5da5d2cad8593b9d0a28a936af79e991a.

--- app/scripts/controllers/newevent.js
+++ app/scripts/controllers/newevent.js
@@ -8,55 +8,6 @@
  * Controller of the eventifyApp
  */
 angular.module('eventifyApp')
-<<<<<<< HEAD
-    .controller('NeweventCtrl', function ($scope, $http, $resource, EventService, $location, AuthService) {
-
-        //this could/should be shared with editevent.js
-        $scope.imageStyle = {
-            'background-color': '#444'
-        };
-
-        $scope.event = {};
-
-        if (AuthService.isLoggedIn()) {
-            $scope.adminID = AuthService.currentUser().id;
-        } else {
-            $location.path('#/');
-        }
-
-        $scope.processForm = function () {
-            EventService.processForm.save({}, {
-
-                name: $scope.event.name,
-                date: $scope.event.date,
-                time: $scope.event.time,
-                locationName: $scope.event.locationName,
-                coverPicture: $scope.event.coverPicture,
-                duration: $scope.event.duration,
-                locationLat: $scope.event.latitudeMap,
-                locationLong: $scope.event.longitudeMap,
-                description: $scope.event.description,
-                spotifyPlaylist: $scope.event.spotifyPlaylist,
-                admin: $scope.adminID
-            }, function (data) {
-                console.log(data.linkId);
-                $location.path('/event/' + data.linkId);
-
-            }, function (data) {
-                console.log(data);
-            });
-        };
-
-        //this could/should be shared with editevent.js
-        $scope.updateCover = function () {
-            $scope.imageStyle = {
-                background: 'linear-gradient( rgba(0, 0, 0,0.5), rgba(0, 0, 0,0.5) ), url(' + $scope.event.coverPicture + ') no-repeat center center',
-                'background-size': 'cover'
-            };
-        };
-
-    });
-=======
   .controller('NeweventCtrl', function ($scope, $http, $resource, EventService, $location, AuthService, $filter) {
 
     //this could/should be shared with editevent.js
@@ -69,7 +20,7 @@ angular.module('eventifyApp')
     if (AuthService.isLoggedIn()) {
       $scope.adminID = AuthService.currentUser().id;
     } else {
-       $location.path('#/');
+      $location.path('#/');
     }
 
     $scope.processForm = function () {
@@ -77,7 +28,7 @@ angular.module('eventifyApp')
 
         name: $scope.event.name,
         date: $scope.event.date,
-        time: $filter('date')($scope.event.time , 'HH:mm:ss'),
+        time: $filter('date')($scope.event.time, 'HH:mm:ss'),
         locationName: $scope.event.locationName,
         coverPicture: $scope.event.coverPicture,
         duration: $scope.event.duration,
@@ -104,4 +55,3 @@ angular.module('eventifyApp')
     };
 
   });
->>>>>>> origin/master
