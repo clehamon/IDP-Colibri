acf79a620a5441301ef796368d7604e84e7720eb
Mikko Honkanen <mikko.s.honkanen@aalto.fi>
2016-04-20 10:46:55 +0200

    removing from tasks and addin people to party started

--- app/scripts/controllers/event.js
+++ app/scripts/controllers/event.js
@@ -8,115 +8,97 @@
  * Controller of the eventifyApp
  */
 angular.module('eventifyApp')
-    .controller('EventCtrl', function ($scope, EventService, $routeParams, AuthService) {
+  .controller('EventCtrl', function ($scope, EventService, $routeParams, AuthService) {
 
-        $scope.linkID = $routeParams.linkID;
-        $scope.editing = false;
+    $scope.linkID = $routeParams.linkID;
+    $scope.editing = false;
 
-        $scope.Spotify = "";
+    $scope.Spotify = "";
 
-        $scope.isLogged = AuthService.isLoggedIn();
+    $scope.isLogged = AuthService.isLoggedIn();
 
-        $scope.user = AuthService.currentUser();
+    $scope.user = AuthService.currentUser();
 
-        $scope.SpotifyURI = "";
+    $scope.SpotifyURI = "";
 
-        //this name should be changed
-        $scope.getEventByID = function () {
-            $scope.eventStatus = true;
-            EventService.Event.get({
-                eventId: $scope.linkID
-            }, function (data) {
-                $scope.event = data;
+    $scope.isAttending = function () {
+      var attending = false;
+      for (var i = 0; i < $scope.event.attendee.length; i++) {
+        if ($scope.event.attendee[i].id == $scope.user.id) {
+          attending = true;
+        }
+      }
+      return attending;
+    }
 
-                if ($scope.isLogged) {
-                    $scope.eventAdmin = ($scope.user.id === $scope.event.admin);
-                }
-                //checking spotify playlist for ng-show
+    //this name should be changed
+    $scope.getEventByID = function () {
+      $scope.eventStatus = true;
+      EventService.Event.get({
+        eventId: $scope.linkID
+      }, function (data) {
+        console.log(data);
+        $scope.event = data;
 
+        if ($scope.isLogged) {
+          $scope.eventAdmin = ($scope.user.id === $scope.event.admin);
+        }
+        //checking spotify playlist for ng-show
 
-                //setting bg picture
-                $scope.bgStyle = {
-                    'background-image': 'url(' + $scope.event.coverPicture + ')'
-                };
-
-                //not sure if this should be in MapCtrl
-                $scope.mapCenter = {
-                    latitude: data.locationLat,
-                    longitude: data.locationLong
-                };
-
-                //This is required by the directive
-                $scope.markerCenter = {
-                    latitude: data.locationLat,
-                    longitude: data.locationLong
-                };
-
-                //couldn't make "{{event.id}}" work directly in the HTML
-
-                $scope.id = data.id;
-                $scope.Spotify = data.spotifyPlaylist;
-
-                console.log(data.spotifyPlaylist);
-
-                $scope.checkPlaylist = function () {
-                    if (data.spotifyPlaylist) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                };
-
-
-                $scope.SpotifyURI = 'https://embed.spotify.com/?uri=' + $scope.Spotify;
-
-
-
-                $scope.attendees = data.attendee;
-
-                $scope.eventStatus = false;
-
-                //finally save data to service
-                EventService.setEventData(data);
-
-            }, function (data) {
-                console.log(data);
-            });
+        //setting bg picture
+        $scope.bgStyle = {
+          'background-image': 'url(' + $scope.event.coverPicture + ')'
         };
 
-
-
-
-        $scope.getUserByID = function (ID) {
-            var user;
-            $scope.attendees.forEach(function (attendee) {
-                if (attendee.id === ID) {
-                    user = attendee;
-                }
-            });
-            return user;
+        //not sure if this should be in MapCtrl
+        $scope.mapCenter = {
+          latitude: data.locationLat,
+          longitude: data.locationLong
         };
 
-        //THIS IS NOT USED
-        $scope.startEditing = function () {
-            $scope.editing = true;
+        //This is required by the directive
+        $scope.markerCenter = {
+          latitude: data.locationLat,
+          longitude: data.locationLong
         };
 
-        //THIS IS NOT USED
-        $scope.stopEditing = function () {
-            $scope.editing = false;
-            EventService.updateEvent.update({}, {
-                id: $scope.event.id,
-                name: $scope.event.name,
-                date: $scope.event.date,
-                time: $scope.event.time,
-                duration: $scope.event.duration,
-                description: $scope.event.description
-            }, function (data) {
-                console.log(data);
-            }, function (data) {
-                console.log(data);
-            });
+        //couldn't make "{{event.id}}" work directly in the HTML
+
+        $scope.id = data.id;
+        $scope.Spotify = data.spotifyPlaylist;
+
+        console.log(data.spotifyPlaylist);
+
+        $scope.checkPlaylist = function () {
+          if (data.spotifyPlaylist) {
+            return true;
+          } else {
+            return false;
+          }
         };
 
-    });
+        $scope.SpotifyURI = 'https://embed.spotify.com/?uri=' + $scope.Spotify;
+
+        $scope.attendees = data.attendee;
+
+        $scope.eventStatus = false;
+
+        //finally save data to service
+        EventService.setEventData(data);
+
+      }, function (data) {
+        console.log(data);
+      });
+    };
+
+    $scope.getUserByID = function (ID) {
+      var user;
+      $scope.attendees.forEach(function (attendee) {
+        if (attendee.id === ID) {
+          user = attendee;
+        }
+      });
+      return user;
+    };
+
+  });
