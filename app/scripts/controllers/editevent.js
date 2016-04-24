a8f94e4e6ce7c4d4d36d16071edb543fce924bd8
Mikko Honkanen <mikko.s.honkanen@aalto.fi>
2016-04-24 15:28:35 +0200

    editevent cover picture

--- app/scripts/controllers/editevent.js
+++ app/scripts/controllers/editevent.js
@@ -12,38 +12,43 @@ angular.module('eventifyApp')
     angular.element(document).ready(function () {
       $scope.event = EventService.getEventData();
       console.log($scope.event);
-    });
 
-
-      $scope.processForm = function () {
-        console.log($('#time-inp').val());
-        
-        //var times = $('#time-inp').val().split(':');
-        
-        EventService.updateEvent.update({}, {
-          id: $scope.event.id,
-          name: $scope.event.name,
-          date: $scope.event.date,
-          time: $('#time-inp').val(),//$filter('date')($scope.event.time , "HH:mm:ss"),
-          locationName: $scope.event.locationName,
-          coverPicture: $scope.event.coverPicture,
-          duration: $scope.event.duration,
-          locationLat: $scope.event.latitudeMap,
-          locationLong: $scope.event.longitudeMap,
-          description: $scope.event.description,
-          spotifyPlaylist: $scope.event.spotifyPlaylist,
-          admin: $scope.adminID
-        }, function (data) {
-          console.log(data);
-          $location.path('/event/' + $scope.event.linkId);
-
-        }, function (data) {
-          console.log(data);
-        });
+      $scope.imageStyle = {
+        background: 'linear-gradient( rgba(0, 0, 0,0.5), rgba(0, 0, 0,0.5) ), url(' + $scope.event.coverPicture + ') no-repeat center center',
+        'background-size': 'cover'
       };
-  
-      $scope.printTime = function(){
-        console.log($scope.event.time);
-      }
+
     });
-  
+
+
+    $scope.processForm = function () {
+      console.log($('#time-inp').val());
+
+      //var times = $('#time-inp').val().split(':');
+
+      EventService.updateEvent.update({}, {
+        id: $scope.event.id,
+        name: $scope.event.name,
+        date: $scope.event.date,
+        time: $('#time-inp').val(), //$filter('date')($scope.event.time , "HH:mm:ss"),
+        locationName: $scope.event.locationName,
+        coverPicture: $scope.event.coverPicture,
+        duration: $scope.event.duration,
+        locationLat: $scope.event.latitudeMap,
+        locationLong: $scope.event.longitudeMap,
+        description: $scope.event.description,
+        spotifyPlaylist: $scope.event.spotifyPlaylist,
+        admin: $scope.adminID
+      }, function (data) {
+        console.log(data);
+        $location.path('/event/' + $scope.event.linkId);
+
+      }, function (data) {
+        console.log(data);
+      });
+    };
+
+    $scope.printTime = function () {
+      console.log($scope.event.time);
+    }
+  });
