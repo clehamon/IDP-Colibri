ecd2adc7eaf46c45d76e140ec5ca573b74b611a4
mohit493 <mohityadav0493@gmail.com>
2016-04-24 18:34:25 +0200

    Map Update
    sharing the place to the data base

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -8,85 +8,93 @@
  * Controller of the eventifyApp
  */
 angular.module('eventifyApp')
-  .controller('MapCtrl', function ($scope, uiGmapGoogleMapApi) {
+    .controller('MapCtrl', function ($scope, uiGmapGoogleMapApi) {
 
-    uiGmapGoogleMapApi.then(function (maps) {
-      console.log(maps);
-    });
+        uiGmapGoogleMapApi.then(function (maps) {
+            console.log(maps);
+        });
 
-    $scope.lat = 52.47491894326404;
-    $scope.lngt = -1.8684210293371217;
-
-    $scope.map = {
-      center: {
-        latitude: $scope.lat,
-        longitude: $scope.lngt
-      },
-      zoom: 15
-    }; //TODO:  set location based on users current gps location 
-    $scope.marker = {
-      id: 0,
-      coords: {
-        latitude: $scope.lat,
-        longitude: $scope.lngt
-      },
-      options: {
-        draggable: true
-      },
-      events: {
-        dragend: function (marker, eventName, args) {
-          console.log(marker, eventName, args);
-          $scope.marker.options = {
-            draggable: true,
-            labelContent: 'lat: ' + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
-            labelAnchor: '100 0',
-            labelClass: 'marker-labels'
-          };
-        }
-      }
-    };
-    var events = {
-      //change back to places_changed
-      places_changed: function (searchBox) {
-
-        var place = searchBox.getPlaces();
-
-
-        if (!place || place === 'undefined' || place.length === 0) {
-          console.log('no place data :(');
-          return;
-        }
-
-        $scope.lat = place[0].geometry.location.lat();
-        $scope.lngt = place[0].geometry.location.lng();
-
-        console.log($scope.lat, "+", $scope.lngt);
-        $scope.event.latitudeMap = $scope.lat;
-        $scope.event.longitudeMap = $scope.lngt;
+        $scope.lat = 52.47491894326404;
+        $scope.lngt = -1.8684210293371217;
 
         $scope.map = {
-          center: {
-            latitude: $scope.lat,
-            longitude: $scope.lngt
-
-          },
-          zoom: 18
-        };
+            center: {
+                latitude: $scope.lat,
+                longitude: $scope.lngt
+            },
+            zoom: 15
+        }; //TODO:  set location based on users current gps location 
         $scope.marker = {
-          id: 0,
-          coords: {
-            latitude: $scope.lat,
-            longitude: $scope.lngt
-          }
+            id: 0,
+            coords: {
+                latitude: $scope.lat,
+                longitude: $scope.lngt
+            },
+            options: {
+                draggable: true
+            },
+            events: {
+                dragend: function (marker, eventName, args) {
+                    console.log(marker, eventName, args);
+                    $scope.marker.options = {
+                        draggable: true,
+                        labelContent: 'lat: ' + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+                        labelAnchor: '100 0',
+                        labelClass: 'marker-labels'
+                    };
+                }
+            }
+        };
+        var events = {
+            //change back to places_changed
+            places_changed: function (searchBox) {
+
+                var place = searchBox.getPlaces();
+                $scope.placeName = "";
+
+
+
+                if (!place || place === 'undefined' || place.length === 0) {
+                    console.log('no place data :(');
+                    return;
+                }
+
+                var placeName = place[0].formatted_address;
+                console.log("from map " + placeName);
+                $scope.event.placeName = placeName;
+                console.log("from event " + $scope.event.placeName);
+
+
+                $scope.lat = place[0].geometry.location.lat();
+                $scope.lngt = place[0].geometry.location.lng();
+
+                console.log($scope.lat, "+", $scope.lngt);
+                $scope.event.latitudeMap = $scope.lat;
+                $scope.event.longitudeMap = $scope.lngt;
+
+                $scope.map = {
+                    center: {
+                        latitude: $scope.lat,
+                        longitude: $scope.lngt
+
+                    },
+                    zoom: 18
+                };
+                $scope.marker = {
+                    id: 0,
+                    coords: {
+                        latitude: $scope.lat,
+                        longitude: $scope.lngt
+                    }
+                };
+
+            }
         };
 
-      }
-    };
-
-    $scope.searchbox = {
-      template: 'searchbox.tpl.html',
-      events: events
-    };
+        $scope.searchbox = {
+            template: 'searchbox.tpl.html',
+            events: events
+        };
 
 
-  });
+    });
--- app/scripts/controllers/newevent.js
+++ app/scripts/controllers/newevent.js
@@ -8,50 +8,54 @@
  * Controller of the eventifyApp
  */
 angular.module('eventifyApp')
-  .controller('NeweventCtrl', function ($scope, $http, $resource, EventService, $location, AuthService, $filter) {
+    .controller('NeweventCtrl', function ($scope, $http, $resource, EventService, $location, AuthService, $filter) {
 
-    //this could/should be shared with editevent.js
-    $scope.imageStyle = {
-      'background-color': '#444'
-    };
+        //this could/should be shared with editevent.js
+        $scope.imageStyle = {
+            'background-color': '#444'
+        };
 
-    $scope.event = {};
 
-    if (AuthService.isLoggedIn()) {
-      $scope.adminID = AuthService.currentUser().id;
-    } else {
-      $location.path('#/');
-    }
 
-    $scope.processForm = function () {
-      EventService.processForm.save({}, {
+        $scope.event = {};
 
-        name: $scope.event.name,
-        date: $scope.event.date,
-        time: $filter('date')($scope.event.time, 'HH:mm:ss'),
-        locationName: $scope.event.locationName,
-        coverPicture: $scope.event.coverPicture,
-        duration: $scope.event.duration,
-        locationLat: $scope.event.latitudeMap,
-        locationLong: $scope.event.longitudeMap,
-        // description: $scope.event.description,
-        spotifyPlaylist: $scope.event.spotifyPlaylist,
-        admin: $scope.adminID
-      }, function (data) {
-        console.log(data.linkId);
-        $location.path('/event/' + data.linkId);
 
-      }, function (data) {
-        console.log(data);
-      });
-    };
+        if (AuthService.isLoggedIn()) {
+            $scope.adminID = AuthService.currentUser().id;
+        } else {
+            $location.path('#/');
+        }
 
-    //this could/should be shared with editevent.js
-    $scope.updateCover = function () {
-      $scope.imageStyle = {
-        background: 'linear-gradient( rgba(0, 0, 0,0.5), rgba(0, 0, 0,0.5) ), url(' + $scope.event.coverPicture + ') no-repeat center center',
-        'background-size': 'cover'
-      };
-    };
+        $scope.processForm = function () {
+            EventService.processForm.save({}, {
 
-  });
+                name: $scope.event.name,
+                date: $scope.event.date,
+                time: $filter('date')($scope.event.time, 'HH:mm:ss'),
+                locationName: $scope.event.locationName,
+                coverPicture: $scope.event.coverPicture,
+                duration: $scope.event.duration,
+                locationLat: $scope.event.latitudeMap,
+                locationLong: $scope.event.longitudeMap,
+                locationName: $scope.event.placeName,
+                description: $scope.event.description,
+                spotifyPlaylist: $scope.event.spotifyPlaylist,
+                admin: $scope.adminID
+            }, function (data) {
+                console.log(data.linkId);
+                $location.path('/event/' + data.linkId);
+
+            }, function (data) {
+                console.log(data);
+            });
+        };
+
+        //this could/should be shared with editevent.js
+        $scope.updateCover = function () {
+            $scope.imageStyle = {
+                background: 'linear-gradient( rgba(0, 0, 0,0.5), rgba(0, 0, 0,0.5) ), url(' + $scope.event.coverPicture + ') no-repeat center center',
+                'background-size': 'cover'
+            };
+        };
+
+    });
